5da6dcbd7bef3c293faed58e285723b9c40092a4
StickOgh <137998639+StickOgh@users.noreply.github.com>
2024-02-24 18:55:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["IanOnTop","Watching TICKETS","Made By Ian4Life!"];
+const statusMessages = ["IanOnTop","Watching TICKETS","Made By Ian4Life!","VadeOnTop"];
 
 
 let currentIndex = 0;
